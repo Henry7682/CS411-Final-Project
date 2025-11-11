744fce207dc477221b2fec229a8bd8455def1234
Sarah Cadet <scadet@bu.edu>
2025-11-11 11:47:47 -0500

    payment classes

---
+++ TerrierThrift/app/src/main/java/com/example/terrierthrift/PaymentCash.java
@@ -0,0 +1,8 @@
+package com.example.terrierthrift;
+
+public class PaymentCash extends PaymentMethod{
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Agreement to pay in cash $" + amount);
+    }
+}
