cb0d0d534b21521eb4d8935a2dc46b151843d41a
lmreigle <lmreigle@bu.edu>
2025-11-13 10:33:24 -0500

    User class (outline)

--- TerrierThrift/app/src/main/java/com/example/terrierthrift/User.java
+++ TerrierThrift/app/src/main/java/com/example/terrierthrift/User.java
@@ -1,8 +1,9 @@
+package com.example.terrierthrift;
 
 import java.util.List;
 import java.util.Map;
 
-public class User{
+public class User {
 
     private int userID;
     private String userName;
@@ -13,7 +14,7 @@ public class User{
     private List<ItemListing> listings;
     //private PaymentMethod paymentType; // not sure if this is necessary, paymentType is specific to each transaction
 
-    public User(int userID, String userName, String email, String name){
+    public User(int userID, String userName, String email, String name) {
         this.userID = userID;
         this.userName = userName;
         this.email = email;
@@ -21,61 +22,68 @@ public class User{
         this.hasAccount = true;
     }
 
-    public int getID() { return userID; }
-    public String getUsername(){ return userName; }
-    public void setUsername(String n){ this.userName = n; }
-    public String getEmail(){ return email; }
-    public void setEmail(String n){ this.email = n; }
-    public String getName(){ return name; }
-    public void setName(String n){ this.name = n; }
-    public List<ItemListing> getListings() { return listings; }
-    public boolean getVerification(){ return isVerifiedUser;}
+    public int getID() {
+        return userID;
+    }
+
+    public String getUsername() {
+        return userName;
+    }
+
+    public void setUsername(String n) {
+        this.userName = n;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String n) {
+        this.email = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        this.name = n;
+    }
+
+    public List<ItemListing> getListings() {
+        return listings;
+    }
+
+    public boolean getVerification() {
+        return isVerifiedUser;
+    }
+
     // User can create a listing with inputted item details and at least one photo. Details is a nested dictionary to organize filters and sub-filter criteria.
-    public ItemListing createListing(List<Photo> photos, Map details){
+    public ItemListing createListing(List<Photo> photos, Map details) {
         return null;
     }
+
     //Initiate a search on the marketplace
-    public SearchEvent startSearch(List<FilterCriteria> criteria){
+    public SearchEvent startSearch(List<FilterCriteria> criteria) {
         // To Do
         return null;
     }
-    public boolean ownsListing(ItemListing item){
+
+    public boolean ownsListing(ItemListing item) {
         return listings.contains(item);
     }
-}
-// choosePaymentMethod
 
-//initiate Transaction
-public static void createTransaction(PaymentType method){
-    // To Do
+    // choosePaymentMethod
 
-}
+    //initiate Transaction
+    public static void createTransaction(PaymentType method) {
+        // To Do
 
-public static void submitReport(String targetType, int targetID, String reason, String details) {
-    // Target type should be either "User" or "listing"
-    // To Do
-}
-
-    /* From class diagram
-
-    class User {
-    -userID: int
-    -username: string
-    -email: string
-    -listings: List<ItemListing>
-    -name: string
-    -hasAccount: boolean
-    -isVerified: boolean
-    -paymentType: PaymentMethod
-    +createListing(details: Map, photos: List<Photo>): ItemListing
-    +getListings(): List<ItemListing>
-    +startSearch(criteria: FilterCriteria): SearchEvent
-    +owns(listing: ItemListing): boolean
-    +choosePaymentMethod(methodType: string)
-    +createTransaction()
-    +submitReport(targetType: string, targetID: int, reason: string, details: string): void
     }
 
+    public static void submitReport(String targetType, int targetID, String reason, String details) {
+        // Target type should be either "User" or "listing"
+        // To Do
+    }
 
-     */
-
+}
